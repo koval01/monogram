e525d50a530508c21b29d79dfa25c900a5163f18
Koval Yaroslav <yaroslav@koval.page>
2023-12-19 11:58:13 +0200

    update language validator

--- misc/lang.py
+++ misc/lang.py
@@ -10,11 +10,35 @@ class Lang:
     def __init__(self) -> None:
         self.file: str = "lang.json"
         
+    @staticmethod
+    def __check_keys(a_keys: list, b_keys: list) -> bool:
+        """Validation of sequence of keys to maintain cleanliness in json"""
+        
+        # if len(a_keys) != len(b_keys):
+        #     return False
+
+        for key_1, key_2 in zip(a_keys, b_keys):
+            if key_1 != key_2:
+                return False
+
+        return True
+
     @property
     def valid(self) -> bool:
+        valid_keys = self.dictionary["valid_lang_keys"]
+
         for key in self.dictionary["keys"]:
-            for l_key in self.dictionary["valid_lang_keys"]:
-                if l_key not in self.dictionary["keys"][key].keys():
+            for l_key in valid_keys:
+
+                kl = self.dictionary["keys"][key].keys()
+                if l_key not in kl:
+                    return False
+
+                for kl_key in kl:
+                    if kl_key not in valid_keys:
+                        return False
+
+                if not self.__check_keys(kl, valid_keys):
                     return False
 
         return True
