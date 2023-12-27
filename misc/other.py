538e19914d3cc7508ded062f8255f012ddae0b32
Koval Yaroslav <yaroslav@koval.page>
2023-12-27 16:44:35 +0200

    update lang misc

--- misc/other.py
+++ misc/other.py
@@ -5,6 +5,10 @@ class Other:
     
     @staticmethod
     def name_translate(lang: str, name: str) -> str:
-        if lang == "uk":
-            return name
+        if lang in ["uk", "ru"]:
+            return {
+                "uk": name,
+                "ru": name.lower().replace("і", "и").title()
+            }[lang]
+
         return translit(name, 'uk', reversed=True)
