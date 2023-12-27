a83fd7d91ffe6106029ffd681c4f74bd1820cf10
Koval Yaroslav <yaroslav@koval.page>
2023-12-27 15:19:17 +0200

    rate limits update in personal actions

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -9,24 +9,22 @@ from handlers.ratelimit import rate_limit
 
 
 @dp.message_handler(commands="start")
-@rate_limit(5, "start")
+@rate_limit(3, "start")
 async def cmd_start(message: types.Message) -> types.Message:
     return await Start(message).process()
 
 
 @dp.message_handler(commands="logout")
-@rate_limit(3, "start")
+@rate_limit(2, "logout")
 async def cmd_logout(message: types.Message) -> types.Message:
     return await LogOut(message).process()
 
 
 @dp.message_handler(is_owner=True, commands="check-proto")
-@rate_limit(3, "start")
 async def cmd_check_proto(message: types.Message) -> types.Message:
     return await CheckProto(message).process()
 
 
 @dp.message_handler(is_owner=True, commands="ping")
-@rate_limit(3, "start")
 async def cmd_ping_bot(message: types.Message) -> types.Message:
     return await message.reply("👊 Up & Running!")
