e00df767c26c1e7b22854db6c0d62963edd6bbdd
Koval Yaroslav <yaroslav@koval.page>
2023-12-27 15:51:47 +0200

    add new_token button to callbacks

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -3,6 +3,12 @@ import re
 from aiogram import types
 from dispatcher import dp
 
-from actions.auth import CheckToken
+from actions.start import Start
 
 from handlers.ratelimit import rate_limit
+
+
+@dp.callback_query_handler(lambda c: c.data == "new_token")
+@rate_limit(3, "new_token_button")
+async def new_token_button(callback_query: types.CallbackQuery):
+    return await Start(callback_query.message).process()
