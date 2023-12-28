0cf47f9a0a28e6b6ff9c5c5af1b79bc2a23e2e49
Koval Yaroslav <yaroslav@koval.page>
2023-12-28 22:52:44 +0200

    add redis set expire key

--- misc/redis_storage.py
+++ misc/redis_storage.py
@@ -19,8 +19,8 @@ class RedisStorage:
         data = await self.cursor.get(key)
         return data.decode("utf-8") if data else None
     
-    async def set(self, key: str, value: str) -> bool:
-        return await self.cursor.set(key, value)
+    async def set(self, key: str, value: str, ex: int = None) -> bool:
+        return await self.cursor.set(key, value, ex=ex)
 
     async def forget(self, key: str) -> bool:
         return await self.cursor.delete(key)
