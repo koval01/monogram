2c7e5c576b11e17428d795f09eb8c3e7d4fd2224
Koval Yaroslav <yaroslav@koval.page>
2024-01-06 17:43:16 +0200

    sys cmd update

--- actions/basic.py
+++ actions/basic.py
@@ -1,5 +1,15 @@
 import sys
+import json
 
+import pydantic
+import redis
+import PIL
+
+import uvloop
+import ujson
+
+import aiohttp
+import aiogram
 from aiogram import types
 
 from misc.mono import Mono
@@ -20,5 +30,18 @@ class Sys:
     def __init__(self, message: types.Message) -> None:
         self.message = message
 
+    @property
+    def sys_info(self) -> str:
+        return json.dumps({
+            "interpreter": sys.version,
+            "aiogram": aiogram.__version__,
+            "aiohttp": aiohttp.__version__,
+            "redis": redis.__version__,
+            "Pillow": PIL.__version__,
+            "uvloop": uvloop.__version__,
+            "ujson": ujson.__version__,
+            "pydantic": pydantic.__version__
+        })
+
     async def process(self) -> types.Message:
-        return await self.message.reply(f"<code>{sys.version}</code>")
+        return await self.message.reply(f"<code>{self.sys_info}</code>")
